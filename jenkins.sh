aaf7f92bb003c7d46ab6397d9a3e86660dfa9ee3
Grimlock, me <grimlock@portnumber53.com>
2019-02-14 10:03:42 -0800

    Changing port to 8180

--- jenkins.sh
+++ jenkins.sh
@@ -14,11 +14,12 @@ if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
     java_opts_array+=( "$item" )
   done < <([[ $JAVA_OPTS ]] && xargs printf '%s\0' <<<"$JAVA_OPTS")
 
-  jenkins_opts_array=( )
+  jenkins_opts_array=( --httpsPort=8180 )
   while IFS= read -r -d '' item; do
     jenkins_opts_array+=( "$item" )
   done < <([[ $JENKINS_OPTS ]] && xargs printf '%s\0' <<<"$JENKINS_OPTS")
 
+echo java "${java_opts_array[@]}" -jar /usr/share/jenkins/jenkins.war "${jenkins_opts_array[@]}" "$@"
   exec java "${java_opts_array[@]}" -jar /usr/share/jenkins/jenkins.war "${jenkins_opts_array[@]}" "$@"
 fi
 
